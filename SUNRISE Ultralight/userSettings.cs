6ef1438837fc632fde702166abbf4ff0f0592e94
EgeLosing <77616044+EgeLosing@users.noreply.github.com>
2022-03-05 12:28:56 +0300

    Add files via upload

---
+++ SUNRISE Ultralight/userSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace SUNRISE_Launcher_UltraLite
+{
+    public static class userSettings
+    {
+        private static string username;
+
+        public static string setUsername
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public static string getUsername
+        {
+            get { return username; }
+        }
+    }
+}
